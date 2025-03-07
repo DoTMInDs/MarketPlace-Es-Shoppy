11ab1cd21fa48595d79af7e55eb86d41784d3864
DoTMInDs <esmondabban3@gmail.com>
2025-03-07 20:57:03 +0000

    fixed build issues

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
-pip install -r requirements.txt
+
 python manage.py collectstatic --noinput
 python manage.py migrate
 
 if [ "$DJANGO_CREATEUSER" == "1" ]; then 
     python mysite/manage.py createsuperuser --noinput
 fi
+
+python marketplace/manage.py runserver 0.0.0.0:$PORT
