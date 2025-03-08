8f62cfa9c73f6c5d90444271541886f7b3b8ac83
DoTMInDs <esmondabban3@gmail.com>
2025-03-08 10:41:48 +0000

    updated ../build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ python manage.py collectstatic --noinput
 python manage.py migrate
 
 if [ "$DJANGO_CREATEUSER" == "1" ]; then 
-    python mysite/manage.py createsuperuser --noinput
+    python marketplace/manage.py createsuperuser --noinput
 fi
 
 python marketplace/manage.py runserver 0.0.0.0:$PORT
